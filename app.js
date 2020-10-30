39b016ffa4ce96c0389d10736726addb3fbf9f41
Dluque001 <darwin.luque.98@gmail.com>
2020-10-30 10:17:16 -0600

    Level 5 - Salting and Hashing

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5')
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 // Set server parameters
 const app = express();
@@ -12,12 +14,24 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Set cookies
+app.use(session({
+    secret: 'This is my secret personally, only mine nobody else!',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Set the database server
 mongoose.connect('mongodb://localhost:27017/userDB', {useUnifiedTopology: true, useNewUrlParser: true});
 
+mongoose.set('useCreateIndex', true);
+
 // Create User schema
 const userSchema = new mongoose.Schema({
-    email: {
+    username: {
         type: String,
         require: [true, 'User requires an email.']
     },
@@ -27,9 +41,15 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 // Create indiviual documents of the User DB in a collection name users, pass the singular user
 const User = mongoose.model('user', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Set the GET requests from the website
 app.get('/', (req, res) => {
@@ -37,38 +57,54 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    res.render('login')
-})
+    if (req.isAuthenticated()) {
+        res.redirect('/secrets');
+    } else {
+        res.render('login')
+    }
+});
 
 app.get('/register', (req, res) => {
     res.render('register')
-})
+});
+
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 // Set the POST requests from the website
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save(err => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets')
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if (err) { console.log(err); }
+        else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            })
         }
-    })
+    })    
 });
 
 app.post('/login', (req, res) => {
-    User.findOne({email: req.body.username}, (err, foundUser) => {
-        if (err) { console.log(err); }
-        else {
-            if (foundUser) {
-                if (foundUser.password === md5(req.body.password)) {
-                    res.render('secrets');
-                }
-            }
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets');
+            });
         }
     })
 })
